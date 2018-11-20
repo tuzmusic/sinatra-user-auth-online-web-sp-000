bd6677943320b83e9ecc77c528825276cac7ddca
Jonathan Tuzman <tuzmusic@gmail.com>
2018-11-20 15:09:28 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
-    binding.pry
+    # binding.pry
     # puts "", params, ""
     @user = User.find_by(email: params['email'], password: params['password'])
     session[:id] = @user.id
